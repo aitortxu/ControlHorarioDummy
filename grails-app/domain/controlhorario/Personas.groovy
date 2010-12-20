9ed1df481aa28a4debde451af45b83e9853f5588
aitor alzola <aitoralzola@MacBook-de-aitor-alzola.local>
2010-12-20 22:14:10 +0100

    pruebas constraints

--- grails-app/domain/controlhorario/Personas.groovy
+++ grails-app/domain/controlhorario/Personas.groovy
@@ -3,5 +3,11 @@ package controlhorario
 class Personas {
 
     static constraints = {
+		nombre(blank: true) 
     }
+
+	String apellidos
+	String nombre
+	String documentoIdentificativo
+	
 }
--- test/unit/controlhorario/PersonasTests.groovy
+++ test/unit/controlhorario/PersonasTests.groovy
@@ -11,7 +11,19 @@ class PersonasTests extends GrailsUnitTestCase {
         super.tearDown()
     }
 
-    void testSomething() {
-
+    void testRecuperamosUnaPersona() {
+		mockDomain(Personas, [new Personas(id:1)])
+		assertTrue Personas.get(1) != null
     }
+	void testConstraints(){
+		mockForConstraintsTests(Personas)
+		def persona = new Personas()		
+		assertFalse persona.validate() 
+		assertEquals "nullable", persona.errors["nombre"]
+		assertEquals "nullable", persona.errors["apellidos"]
+		assertEquals "nullable", persona.errors["documentoIdentificativo"]
+		
+		persona.nombre =""
+		assertEquals "blank", persona.errors["nombre"]
+	}
 }
